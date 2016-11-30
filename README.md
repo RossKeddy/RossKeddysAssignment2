804a93c78a4774dd55a2ba9d3e9ce48743e229a4
Leonzola <leonzola7@gmail.com>
2016-11-30 15:17:06 -0500

    Submission Revision 1.0

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RossKeddysAssignment2
+# User Administration as extra feature, just read my documentation
--- config/globalVars.js
+++ config/globalVars.js
@@ -8,8 +8,8 @@ module.exports = {
 	  facebook: {
 		clientID: '1160831130633235',
 		clientSecret: '75bd10c5d5096c2c6d888bafceb5653d',
-		callbackURL: 'http://localhost:3000/facebook/callback'
+		callbackURL: 'https://rosskeddyassignment2.herokuapp.com/facebook/callback'
 	  }
   }
 };
-/*https://rosskeddyassignment2.herokuapp.com/facebook/callback*/
+/*http://localhost:3000/facebook/callback*/
